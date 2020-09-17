973abce3d3940d4b878ec504dd757e6cd0653b1f
olaliga <iphone11134@gmail.com>
2020-09-17 13:40:10 +0800

    first commit

--- prepare_data.R
+++ prepare_data.R
@@ -1,9 +1,97 @@
 
+library("Mass")
+library('ggplot')
+
+#categorical data please use factor type
+# numerical data please use numeric type
+
+#example_data 'iris'
+
+data('iris')
+iris
+
+
+#gets all variables type
+type_col<- function(df)
+{
+  x = c()
+  num <- c()
+  cate <- c()  
+  
+  for( i in c( 1:length(df) ) )
+  {
+    dum = class( df[1,i] )
+    x[i] <- dum 
+    
+    if( dum == 'factor' )
+    {
+      cate[ length(cate) + 1 ] = i
+    }
+    else
+    {
+      num[ length(num) + 1 ] = i
+    }
+  }
+  
+  class_of_var = factor(x)
+  
+  need = list(class = class_of_var, numeric = num, categorical = cate)
+  return(need)
+}
+
+
+#EDA
+
+
+# input: dataframe, output:plot
+
+plot(df)
+{
+  num_col = type_col(df)[ 'numeric' ]
+  cat_col = type_col(df)[ 'categorical' ]
+  
+  # single variable: num - boxplot 
+  
+  
+  # muliti-variable
+    #pair plot
+  
+  
+  
+  return
+}
+
+
+
 # 
-def train_test_split():
+data_summary()
+{
+   return 
+}
+
+
+
+# sample
+
+
+train_test_split()
+{
+  
+  
+  return 
+}
 
 
   
   
+#  
+cross_validation()
+{
+  
+  return
+}                       
+                       
+  
+
   
   
